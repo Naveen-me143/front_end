076180230507e06608d0b03b2c3347bf164744f1
Dasari Naveen <dasarinaveen890@gmail.com>
2025-04-29 19:10:37 +0530

    text

---
+++ React/project_2/src/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const First=()=>{
+    return(
+        <>
+            <h1>hello😂</h1>
+        </>
+    );
+   
+}
+export default First;
--- React/project_2/src/main.jsx
+++ React/project_2/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import First from './FirstComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <First/>
   </StrictMode>,
 )
